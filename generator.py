93a408345dbf3e9380a7f94e2b5b5a2f65cc9376
fuzzyhead8 <43263065+fuzzyhead8@users.noreply.github.com>
2020-03-12 23:26:24 +0100

    gen test2

--- generator.py
+++ generator.py
@@ -1,10 +1,14 @@
 # Initialize the list
-my_list = [1, 3, 6, 10]
+my_list = ['1', '3', '6', '10']
 
 # square each term using list comprehension
 # Output: [1, 9, 36, 100]
-[x**2 for x in my_list]
+a = [str(x) for x in my_list]
+r1 = ",".join(a)
+print (r1)
 
 # same thing can be done using generator expression
 # Output: <generator object <genexpr> at 0x0000000002EBDAF8>
-(x**2 for x in my_list)
+a = (str(x) for x in my_list)
+r2 = ",".join(a)
+print (r2)
